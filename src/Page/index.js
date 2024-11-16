9a57048c9fca674a0ef076591782dde381771cba
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2024-11-16 13:02:59 +0900

    feat : 머지

--- src/Page/index.js
+++ src/Page/index.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Hospital from "./hospital";
+import Medic from "./medic";
 
 const Page = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Hospital />} />
+        <Route path="/medic" element={<Medic />} />
       </Routes>
     </>
   );
--- src/Page/medic/index.js
+++ src/Page/medic/index.js
@@ -1,14 +1,13 @@
 import React from "react";
-import { Container, HospitalCard } from ".style";
+import { Container, HospitalCard } from "./style";
+
 const MedicPage = () => {
   return (
     <>
       <aside> side </aside>
 
       <Container>
-        <hospitalCard>
-          asd
-        </hospitalCard>
+        <hospitalCard>asd</hospitalCard>
       </Container>
     </>
   );
