6c15668408671821d4bf7998444fa6214d4ffe55
RyuDongHo <thffhahs03@gmail.com>
2024-11-16 17:30:24 +0900

    쿠키 추가

--- src/Page/home/index.js
+++ src/Page/home/index.js
@@ -9,6 +9,7 @@ const HomePage = () => {
   const hpRef = React.useRef("");
   const lonRef = React.useRef(0);
   const latRef = React.useRef(0);
+  const hospitalRef = React.useRef("");
   
   deleteAllCookies();
 
@@ -19,6 +20,7 @@ const HomePage = () => {
       <input type="text" placeholder="전화번호 입력" ref={hpRef} />
 
       <h3>병원의 경우 위도/경도를 입력해주세요</h3>
+      <input type="text" placeholder="병원 이름" ref={hospitalRef}/>
       <input type="text" placeholder="longtitude" ref={lonRef} />
       <input type="text" placeholder="latitude" ref={latRef} />
       <BtnContainer>
@@ -27,6 +29,9 @@ const HomePage = () => {
           type="button"
           onClick={() => {
             setCookie("hp", hpRef.current.value, { path: "/" });
+            setCookie("lon", lonRef.current.value, { path: "/" });
+            setCookie("lat", latRef.current.value, { path: "/" });
+            setCookie("hospital", hospitalRef.current.value, { path: "/" });
             navigate("/hospital");
           }}
         >
